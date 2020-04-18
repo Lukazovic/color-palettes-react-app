5853673a252f57f39f6e8395fff6474b8ecf5d29
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-18 19:09:21 -0300

    Add link from MiniPalette to Palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import MiniPalette from "./MiniPalette";
-import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
 
 const styles = {
@@ -34,6 +33,9 @@ const styles = {
 };
 
 class PaletteList extends Component {
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
+  }
   render() {
     const { palettes, classes } = this.props;
     return (
@@ -44,7 +46,10 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
-              <MiniPalette {...palette} />
+              <MiniPalette
+                {...palette}
+                handleClick={() => this.goToPalette(palette.id)}
+              />
             ))}
           </div>
         </div>
