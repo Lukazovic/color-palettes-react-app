aa359c6d19df48fb89f517412fb6d0f45423476b
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-19 19:26:18 -0300

    Move styles to other files and folders

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,47 +4,7 @@ import Navbar from "./Navbar";
 import ColorBox from "./ColorBox";
 import PaletteFooter from "./PaletteFooter";
 import { withStyles } from "@material-ui/styles";
-
-const styles = {
-  Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    overflow: "hidden",
-  },
-  colors: {
-    height: "90%",
-  },
-  goBack: {
-    width: "20%",
-    height: "50%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-4px",
-    opacity: "1",
-    backgroundColor: "black",
-    "& a": {
-      color: "white",
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      display: "inline-block",
-      top: "50%",
-      left: "50%",
-      marginLeft: "-50px",
-      marginTop: "-15px",
-      textAlign: "center",
-      outline: "none",
-      background: "rgba(255, 255, 255, 0.3)",
-      fontSize: "1rem",
-      lineHeight: "30px",
-      textTransform: "uppercase",
-      // cursor: "pointer",
-    },
-  },
-};
+import styles from "./styles/PaletteStyles";
 
 class SingleColorPalette extends Component {
   constructor(props) {
---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,13 @@
+export default {
+  PaletteFooter: {
+    backgroundColor: "white",
+    height: "5vh",
+    display: "flex",
+    justifyContent: "flex-end",
+    fontWeight: "bold",
+  },
+  emoji: {
+    fontSize: "1rem",
+    margin: "0 0.5rem",
+  },
+};
