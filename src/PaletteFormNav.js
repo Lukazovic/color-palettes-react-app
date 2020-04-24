c219467b953ccff5e970aca8df25a6e224c9b153
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-24 17:09:25 -0300

    Removing unnescessary import

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import PaletteMetaForm from "./PaletteMetaForm";
 import classNames from "classnames";
